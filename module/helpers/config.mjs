c2333c306de74305089dd0838e59578c8e133641
bittersweetpuff <biszkoptex@gmail.com>
2024-01-06 14:26:46 +0100

    Boilerplate system setup

---
+++ module/helpers/config.mjs
@@ -0,0 +1,23 @@
+export const ELECTRICEMBRACE = {};
+
+/**
+ * The set of Ability Scores used within the sytem.
+ * @type {Object}
+ */
+ ELECTRICEMBRACE.abilities = {
+  "str": "ELECTRICEMBRACE.AbilityStr",
+  "dex": "ELECTRICEMBRACE.AbilityDex",
+  "con": "ELECTRICEMBRACE.AbilityCon",
+  "int": "ELECTRICEMBRACE.AbilityInt",
+  "wis": "ELECTRICEMBRACE.AbilityWis",
+  "cha": "ELECTRICEMBRACE.AbilityCha"
+};
+
+ELECTRICEMBRACE.abilityAbbreviations = {
+  "str": "ELECTRICEMBRACE.AbilityStrAbbr",
+  "dex": "ELECTRICEMBRACE.AbilityDexAbbr",
+  "con": "ELECTRICEMBRACE.AbilityConAbbr",
+  "int": "ELECTRICEMBRACE.AbilityIntAbbr",
+  "wis": "ELECTRICEMBRACE.AbilityWisAbbr",
+  "cha": "ELECTRICEMBRACE.AbilityChaAbbr"
+};
