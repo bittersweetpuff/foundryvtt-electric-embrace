a401aac4ed1c58cced4fa5d43029c9f28b224493
bittersweetpuff <biszkoptex@gmail.com>
2024-03-31 22:29:55 +0200

    Skill rolls added (with dialog)

--- module/roller/dialog2d20.mjs
+++ module/roller/dialog2d20.mjs
@@ -1,3 +1,5 @@
+import { roll2d20 } from "./roller2d20.mjs";
+
 export class Dialog2d20 extends Dialog {
   constructor(
     rollName,
@@ -22,10 +24,71 @@ export class Dialog2d20 extends Dialog {
     this.rollLocation = rollLocation;
     this.actor = actor;
     this.item = item;
+    this.deferred = new Deferred();
   };
 
-  activatreListeners(html) {
+  activateListeners(html) {
     super.activateListeners(html);
+
+    this.data.buttons.roll.callback=this.rollButton.bind(this);
+  };
+
+  rollButton(){
+    console.log("ROLLING 2D20 Lets goooo!");
+    let attr = this.element.find('[name="attribute"]').val();
+    let skill = this.element.find('[name="skill"]').val();
+    let complication = this.element.find('[name="complication"]').val();
+    let isTag = this.element.find('[name="tag"]').is(":checked");
+    let bonusDice = this.element.find('[name="bonus dice"]').val();
+
+    //Calculating targetNumber
+    let targetNumber = parseInt(attr) + parseInt(skill);
+
+    //Calculating double range
+    let doubleRange = 1;
+    if (isTag) {
+      doubleRange = skill;
+    }
+
+    //Calculating complication range (TODO REMOVE THIS AND CHANGE IT IN ROLLER!)
+    let complicationRange = 21 - parseInt(complication);
+
+    const rollOptions = {
+      targetNumber: parseInt(targetNumber) || 10,
+      extraDice: parseInt(bonusDice) || 0,
+      difficulty: 1,
+      doubleRange: parseInt(doubleRange) || 1,
+      complicationRange: parseInt(complicationRange) || 1,
+    };
+
+    const result = roll2d20(rollOptions);
+
+    const chatData = {
+      ...rollOptions,
+      ...result,
+    };
+
+    renderTemplate('systems/electricembrace/templates/chat-cards/2d20-chat-card.html', chatData).then(
+      (html) => {
+        const messageData = {
+          user: game.user_id,
+          type: CONST.CHAT_MESSAGE_TYPES.ROLL,
+          content: html,
+        };
+
+        console.log(html);
+
+        const rollMode = game.settings.get('core', 'rollMode');
+        ChatMessage.applyRollMode(messageData, rollMode);
+
+        //CONFIG.ChatMessage.entityClass.create(messageData);
+        ChatMessage.create(messageData).then(msg => {
+          return msg;
+        });
+      }
+    );
+
+    console.log(result);
   };
 
   static async createDialog( {rollName = "Roll D20", diceNum = 2, attribute = 0, skill = 0, tag = false, complication = 20, rollLocation = false, actor = null, item = null} = {}) {
@@ -40,24 +103,38 @@ export class Dialog2d20 extends Dialog {
     dialogData.rollLocation = rollLocation;
     dialogData.actor = actor;
     dialogData.item = item;
-    
+
     const html = await renderTemplate("systems/electricembrace/templates/dialogs/dialog2d20.html", dialogData);
     let d = new Dialog2d20(
-			rollName,
-			diceNum,
-			attribute,
-			skill,
-			tag,
-			complication,
-			rollLocation,
-			actor,
-			item,
-			{
-				title: rollName,
-				content: html,
-				buttons: {},
-			}
-		);
-		d.render(true);
+      rollName,
+      diceNum,
+      attribute,
+      skill,
+      tag,
+      complication,
+      rollLocation,
+      actor,
+      item,
+      {
+        title: rollName,
+        content: html,
+        buttons: {
+          roll: {
+            label: "ROLL",
+          },
+        },
+      },
+    );
+    d.render(true);
+    return d.deferred.promise;
+  }
+}
+
+class Deferred {
+  constructor() {
+    this.promise = new Promise((resolve, reject) => {
+      this.reject = reject;
+      this.resolve = resolve;
+    });
   }
 }
