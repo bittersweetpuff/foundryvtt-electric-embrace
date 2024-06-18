bef8d63eeb9c698e442d66557826d09843320fce
bittersweetpuff <biszkoptex@gmail.com>
2024-06-18 19:05:12 +0200

    Dialog D6

--- hooks/diceSoNiceReadyHook.mjs
+++ hooks/diceSoNiceReadyHook.mjs
@@ -3,4 +3,80 @@ export function diceSoNiceReadyHook(dice3d) {
 
   dice3d.addSystem({ id: "electricembrace", name: "Electric Embrace" }, true);
 
+  dice3d.addColorset({
+		name: "electricembrace",
+		description: "Electric Embrace",
+		category: "Colors",
+		foreground: "#fcef71",
+		background: "#008cd1",
+		outline: "gray",
+		texture: "none",
+	});
+
+  dice3d.addColorset({
+		name: "succuban",
+		description: "Succuban Pink",
+		category: "Colors",
+		foreground: "#ffe8e8",
+		background: "#d40055",
+		outline: "gray",
+		texture: "none",
+	});
+
+  dice3d.addColorset({
+		name: "papaj",
+		description: "Papaj 202",
+		category: "Colors",
+		foreground: "#d59e4a",
+		background: "#fcf6d2",
+		outline: "gray",
+		texture: "none",
+	});
+
+	dice3d.addDicePreset({
+		type: "dc",
+		labels: [
+			"systems/electricembrace/assets/dice/d1.webp",
+			"systems/electricembrace/assets/dice/d2.webp",
+			"systems/electricembrace/assets/dice/d3.webp",
+			"systems/electricembrace/assets/dice/d4.webp",
+			"systems/electricembrace/assets/dice/d5.webp",
+			"systems/electricembrace/assets/dice/d6.webp",
+		],
+		system: "electricembrace",
+
+	});
+
+	dice3d.addDicePreset({
+		type: "dh",
+		fontScale: 0.9,
+		labels: [
+			"systems/electricembrace/assets/dice-locations/head.webp",
+			"systems/electricembrace/assets/dice-locations/head.webp",
+			"systems/electricembrace/assets/dice-locations/body.webp",
+			"systems/electricembrace/assets/dice-locations/body.webp",
+			"systems/electricembrace/assets/dice-locations/body.webp",
+			"systems/electricembrace/assets/dice-locations/body.webp",
+			"systems/electricembrace/assets/dice-locations/body.webp",
+			"systems/electricembrace/assets/dice-locations/body.webp",
+			"systems/electricembrace/assets/dice-locations/arm-l.webp",
+			"systems/electricembrace/assets/dice-locations/arm-l.webp",
+			"systems/electricembrace/assets/dice-locations/arm-l.webp",
+			"systems/electricembrace/assets/dice-locations/arm-r.webp",
+			"systems/electricembrace/assets/dice-locations/arm-r.webp",
+			"systems/electricembrace/assets/dice-locations/arm-r.webp",
+			"systems/electricembrace/assets/dice-locations/leg-l.webp",
+			"systems/electricembrace/assets/dice-locations/leg-l.webp",
+			"systems/electricembrace/assets/dice-locations/leg-l.webp",
+			"systems/electricembrace/assets/dice-locations/leg-r.webp",
+			"systems/electricembrace/assets/dice-locations/leg-r.webp",
+			"systems/electricembrace/assets/dice-locations/leg-r.webp",
+		],
+		system: "electricembrace",
+		colorset: "electricembrace",
+	});
+
+
 };
+
+
---
+++ module/roller/DialogD6.mjs
@@ -0,0 +1,80 @@
+export class DialogD6 extends Dialog {
+
+	constructor(rollName, diceNum, actor, weapon, EERoll, dialogData = {}, options = {}) {
+		super(dialogData, options);
+		this.rollName = rollName;
+		this.diceNum = diceNum;
+		this.actor = actor;
+		this.weapon = weapon;
+		this.EERoll = EERoll;
+		this.options.classes = ["dice-icon"];
+	}
+
+	activateListeners(html) {
+		// Check when the box is changed if actor has enough ammo
+		super.activateListeners(html);
+		// html.on('change', '.d-number', async (e, i, a) => {
+		//     await this.checkAmmo(html)
+		// })
+
+		html.on("click", ".roll", async event => {
+			let diceNum = html.find(".d-number").value;
+
+			if (!this.falloutRoll) {
+				fallout.Roller2D20.rollD6({
+					rollname: this.rollName,
+					dicenum: parseInt(diceNum),
+					weapon: this.weapon,
+					actor: this.actor,
+				});
+			}
+			else {
+				fallout.Roller2D20.addD6({
+					rollname: this.rollName,
+					dicenum: parseInt(diceNum),
+					weapon: this.weapon,
+					actor: this.actor,
+					falloutRoll: this.falloutRoll,
+				});
+			}
+		});
+	}
+
+	async rollD6() {
+
+	}
+
+	async addD6() {
+
+	}
+
+	static async createDialog({ rollName = "DC Roll", diceNum = 2, EERoll = null, actor= null, weapon = null } = {}) {
+		let dialogData = {};
+
+		dialogData.rollName = rollName;
+		dialogData.diceNum = diceNum;
+		dialogData.EERoll = EERoll;
+		dialogData.weapon = weapon;
+		dialogData.actor = actor;
+
+		const html = `<div class="flexrow electricembrace-dialog">
+		<div class="flexrow resource" style="padding:5px">
+		<label class="title-label">Number of Dice:</label><input type="number" class="d-number" value="${diceNum}">
+		</div>
+		</div>`;
+
+		let d = new DialogD6(rollName, diceNum, actor, weapon, EERoll, {
+			title: rollName,
+			content: html,
+			buttons: {
+				roll: {
+					icon: '<i class="fas fa-check"></i>',
+					label: "ROLL",
+				},
+			},
+			close: () => { },
+		});
+		d.render(true);
+	}
+
+}
